d514cef272442344ea9af942932f893776cb613b
Angelo Licetti León <angelo.licetti.00@gmail.com>
2020-07-11 11:53:27 -0500

    implemented Oauth

--- configMensaje.js
+++ configMensaje.js
@@ -3,8 +3,12 @@ module.exports = (formulario) => {
   var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'credismart.peru@gmail.com', // Cambialo por tu email
-      pass: 'nico0227.' // Cambialo por tu password
+      type: 'OAuth2',
+      user: 'credismart.peru@gmail.com',
+      clientId: '910843602766-3g43edogah7ukb7tvpd9a7v9ujl209tu.apps.googleusercontent.com',
+      clientSecret: 'UlGhAazA9UnH6o8gLRC04JVV',
+      refreshToken: '1//04vnu7KXBw49ICgYIARAAGAQSNwF-L9IreSJzFUBuuThQKUmfCC01pcHWOPYnfwA96jwrMcVJsfGcnDrjPrV4-U2OwebT4ulVTKg',
+      accessToken: 'ya29.a0AfH6SMCSHNbjf98nDFkW4PFTEeAivg30yC6p5E3682Dr_EzINiWxFdwnXc6AjgLhZ41Y-5EHp7TUtu7V9NTTbR419okQylKXOOSBvdbeYONI-af7U-zCWOu-vUp_ff7MkoOg0SpyP1i3kn-GGCETTeiLGCbPlEZYacQ'
     }
   });
   const mailOptions = {
